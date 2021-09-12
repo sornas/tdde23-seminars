ad2c0106067a2bce103ed5daf5bdad23e3f260a7
Gustav Sörnäs <gustav@sornas.net>
2021-09-13 01:03:16 +0200

    find_uncut list comprehension

--- sem02/lösningar/find_uncut.py
+++ sem02/lösningar/find_uncut.py
@@ -1,13 +1,28 @@
 def find_uncut(grass):
     uncut = []
-    for y in range(len(grass)):
-        for x in range(len(grass[y])):
+    h = len(grass)
+    w = len(grass[0])  # assume square
+    for y in range(h):
+        for x in range(w):
             if not grass[y][x]:
                 uncut.append((x, y))
     return uncut
 
 
+def find_uncut2(grass):
+    h = len(grass)
+    w = len(grass[0])
+    return [(x, y) for y in range(h) for x in range(w) if not grass[y][x]]
+
+
 print(find_uncut([
+    [True, False, True, False],
+    [False, False, True, True],
+    [True, False, False, True],
+    [False, True, True, True],
+]))
+
+print(find_uncut2([
     [True, False, True, False],
     [False, False, True, True],
     [True, False, False, True],
